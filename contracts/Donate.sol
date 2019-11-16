ff854b840f74c10e3fc3f06da95c6f801b33a4b9
Mallory LeeWong <m.c.leewong@gmail.com>
2019-11-16 14:25:39 -0500

    refactor: added determinePayout oracle function to Donate contract

---
+++ contracts/Donate.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.5.0;
+
+contract Donate {
+  bool payout = false;
+  address donor;
+
+  mapping(address => uint) public deposits;
+
+  constructor() public {
+    donor = msg.sender;
+  }
+
+  function deposit(address charity) public payable {
+    require(msg.sender == donor, "Only the donor can deposit donations.");
+    uint amount = msg.value;
+    require(amount > 0.01 ether, "Minimum donation required is 0.01 ether.");
+    deposits[charity] = deposits[charity] + amount;
+  }
+
+  function determinePayout(uint seismicity, address oracle) public {
+    require(msg.sender == oracle, "Only the oracle can determine the payout");
+    if (seismicity > 7) {
+      payout = true;
+    } else {
+      payout = false;
+    }
+  }
+
+  function withdraw(address payable charity) public {
+    require(payout == true, "Your donation will be sent once an earthquake of at least 7.0 is reported.");
+    require(msg.sender == charity, "Only the designated charity can withdraw the donation.");
+    uint payment = deposits[charity];
+    deposits[charity] = 0;
+    charity.transfer(payment);
+  }
+}
+
+// contract SimpleStorage {
+//   uint storedData;
+
+//   function set(uint x) public {
+//     storedData = x;
+//   }
+
+//   function get() public view returns (uint) {
+//     return storedData;
+//   }
+// }
