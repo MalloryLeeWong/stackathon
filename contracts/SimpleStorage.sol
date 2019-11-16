674d5897b643ea5bc68f0e5f9bac122fa2d8fbc7
Mallory LeeWong <m.c.leewong@gmail.com>
2019-11-16 13:40:14 -0500

    feat: Donate contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -14,34 +14,25 @@ pragma solidity ^0.5.0;
 
 contract Donate {
   bool payout = false;
-  address payable public charity;
-  address public donor;
+  address donor;
 
-  mapping(address => uint256) public deposits;
-
-  modifier onlyCharity() {
-    require(msg.sender == charity, "Recipient not authorized.");
-      _;
-  }
+  mapping(address => uint) public deposits;
 
   constructor() public {
     donor = msg.sender;
-    // donor is assigned address of who is attempting to invoke the contract
   }
 
-  // when user wants to donate to contract, require minimum
-  function deposit() public payable {
+  function deposit(address charity) public payable {
+    require(msg.sender == donor, "Only donor can deposit donation.");
     uint amount = msg.value;
     require(amount > 0.01 ether, "Minimum donation required is 0.01 ether.");
     deposits[charity] = deposits[charity] + amount;
   }
-  function withdrawPayout () public onlyCharity {
-    // this function can only be invoked if payout bool is true
-    require(payout == true, "Your donation will be transferred after a natural disaster is reported.");
-    // require(msg.sender == charity);
+  function withdraw(address payable charity) public {
+    require(payout == true, "Your donation will be transferred after a natural disaster.");
+    require(msg.sender == charity, "Only charity can withdraw donation.");
     uint payment = deposits[charity];
     deposits[charity] = 0;
-    payout = false;
     charity.transfer(payment);
   }
 }
